813e29a361fd12a18622eb6d1c47952174a17e05
Kosh <kosh20111@gmail.com>
2021-09-19 01:29:27 +0200

    add github release and EasyVersion plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,52 @@
 import java.net.URL
-//import com.github.k0shk0sh.easy.version.SnapshotDefinition
+import java.util.concurrent.Callable
+import com.github.k0shk0sh.easy.version.SnapshotDefinition
 
 plugins {
   `kotlin-dsl`
   id("org.jetbrains.dokka") version "1.5.0"
   id("com.vanniktech.maven.publish") version "0.18.0"
-//  id("com.github.k0shk0sh.easy.version") version "+" // get latest always
+  id("com.github.breadmoirai.github-release") version "2.2.12"
+  id("com.github.k0shk0sh.easy.version") version "+" // get latest always
 }
 
-//easyVersion {
-//  snapshotLabel = "-SNAPSHOT"
-//  snapshotDefinition = SnapshotDefinition.TIMESTAMP
-//  propertiesToSet = listOf("VERSION_NAME")
-//  setToProjectVersion = true
-//  logVersion = true
-//}
+easyVersion {
+  snapshotLabel = "-SNAPSHOT"
+  snapshotDefinition = SnapshotDefinition.TIMESTAMP
+  propertiesToSet = listOf("VERSION_NAME")
+  setToProjectVersion = true
+  logVersion = true
+}
+
+githubRelease {
+  val version = Callable<CharSequence> { project.version.toString() }
+  token(System.getenv("GITHUB_TOKEN"))
+  owner("k0shk0sh")
+  repo("EasyVersion")
+  tagName(version)
+  targetCommitish("main")
+  releaseName(version)
+  body(changelog())
+}
+
+tasks.dokkaHtml.configure {
+  dokkaSourceSets {
+    named("main") {
+      failOnWarning.set(true)
+      reportUndocumented.set(true)
+      skipEmptyPackages.set(true)
+      skipDeprecated.set(true)
+      noAndroidSdkLink.set(true)
+      jdkVersion.set(8)
+      sourceLink {
+        localDirectory.set(project.file("src/main/kotlin"))
+        remoteUrl.set(URL("https://github.com/k0shk0sh/EasyVersion/blob/main/src/main/kotlin"))
+        remoteLineSuffix.set("#L")
+      }
+    }
+  }
+  outputDirectory.set(file("docs"))
+}
 
 gradlePlugin {
   plugins {
@@ -36,23 +68,4 @@ dependencies {
   kotlin("kotlin-gradle")
   implementation(gradleApi())
   implementation("com.google.code.gson:gson:2.8.8")
-}
-
-tasks.dokkaHtml.configure {
-  dokkaSourceSets {
-    named("main") {
-      failOnWarning.set(true)
-      reportUndocumented.set(true)
-      skipEmptyPackages.set(true)
-      skipDeprecated.set(true)
-      noAndroidSdkLink.set(true)
-      jdkVersion.set(8)
-      sourceLink {
-        localDirectory.set(project.file("src/main/kotlin"))
-        remoteUrl.set(URL("https://github.com/k0shk0sh/EasyVersion/blob/main/src/main/kotlin"))
-        remoteLineSuffix.set("#L")
-      }
-    }
-  }
-  outputDirectory.set(file("docs"))
 }
